d3807aeab01d3075f9a3f5ef51dc216adfbe611f
loc phan <phanhoangloc2017@gmail.com>
2020-12-26 15:38:54 +0700

    Hoan thanh chuc nang Login Admin

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,9 @@ Route::get('/', function () {
 Route::group(['prefix'=>'admin'],function(){
 
     
+    Route::get('login','LoginController@create');
+    Route::post('login','LoginController@store');
+    Route::get('logout','LoginController@logout');
 });
 
 
